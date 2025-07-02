d7ce8e0ce23e2ffcc5aa3de1c5aee3de7b120d98
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-02 16:33:17 +0530

    Update p3_eval.py

--- pages/p3_eval.py
+++ pages/p3_eval.py
@@ -1,6 +1,6 @@
 import streamlit as st
+from pages.code_to_import.p3_crosscheck_resume_jd import resume_x_jd
 from styles import css_dark
 
 st.markdown(css_dark, unsafe_allow_html=True)
-st.title("🧮 Evaluate Applications")
-st.write("This page will allow you to compare resumes with job descriptions.") 
+resume_x_jd() 
