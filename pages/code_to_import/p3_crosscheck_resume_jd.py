bbecaaa82ec1589ec7c808db1db9b14950ed2c97
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 01:17:05 +0530

    Update p3_crosscheck_resume_jd.py

--- pages/code_to_import/p3_crosscheck_resume_jd.py
+++ pages/code_to_import/p3_crosscheck_resume_jd.py
@@ -1,3 +1,30 @@
+import streamlit as st
+import os
+import git
+from docx import Document
+from pypdf import PdfReader
+from streamlit_tree_select import tree_select
+from styles import css_dark
+
+st.markdown(css_dark, unsafe_allow_html=True)
+st.markdown("""
+<style> 
+@media (prefers-color-scheme: dark) {
+  [data-testid="stTreeSelect"] span,
+  [data-testid="stTreeSelect"] label,
+  [data-testid="stTreeSelect"] div {
+    color: #fff !important;
+  }
+  [data-testid="stTreeSelect"] input[type="checkbox"] {
+    filter: invert(1); /* Optional: makes the checkbox white */
+  }
+} 
+</style>
+""", unsafe_allow_html=True)
+
+def crosscheck_button():
+  st.page_link("pages/p3_eval.py", label="☯️ Evaluate Resume")
+
 def resume_x_jd():
   pass
 
