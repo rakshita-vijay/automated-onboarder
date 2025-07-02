513362d672d4d0226a92145b942904d3b12d20b9
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-02 16:33:28 +0530

    Create p3_crosscheck_resume_jd.py

---
+++ pages/code_to_import/p3_crosscheck_resume_jd.py
@@ -0,0 +1,10 @@
+def resume_x_jd():
+  pass
+
+
+
+""" 
+st.markdown(css_dark, unsafe_allow_html=True)
+st.title("🧮 Evaluate Applications")
+st.write("This page will allow you to compare resumes with job descriptions.")  
+"""
