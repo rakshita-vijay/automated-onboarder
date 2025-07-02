7d13b666871702ccf46c59feaa632ca2044f5406
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 01:34:37 +0530

    Update styles.py

--- styles.py
+++ styles.py
@@ -116,7 +116,7 @@ css_dark = """
 /* Make st.page_link look like outlined buttons */
 .stPageLink {
     display: block; 
-    max-width: 350px; 
+    max-width: 300px; 
     margin: 8px 0;
     border: 2px solid #fff;
     border-radius: 10px;
