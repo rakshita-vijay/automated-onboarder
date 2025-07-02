e24ea6bb15986788b6484a07758aa47d64454428
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-02 16:24:22 +0530

    Create p3_eval.py

---
+++ pages/p3_eval.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from styles import css_dark
+
+st.markdown(css_dark, unsafe_allow_html=True)
+st.title("🧮 Evaluate Applications")
+st.write("This page will allow you to compare resumes with job descriptions.") 
