788df04e6441a9f6931b9b88300443818a98384c
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 01:15:01 +0530

    Update p1_resume.py

--- pages/p1_resume.py
+++ pages/p1_resume.py
@@ -5,6 +5,21 @@ from pages.code_to_import.p1_upload_resume import upload_resume
 from pages.code_to_import.p2_upload_jd import jd_button 
 
 st.markdown(css_dark, unsafe_allow_html=True)
+st.markdown("""
+<style> 
+@media (prefers-color-scheme: dark) {
+  [data-testid="stTreeSelect"] span,
+  [data-testid="stTreeSelect"] label,
+  [data-testid="stTreeSelect"] div {
+    color: #fff !important;
+  }
+  [data-testid="stTreeSelect"] input[type="checkbox"] {
+    filter: invert(1); /* Optional: makes the checkbox white */
+  }
+} 
+</style>
+""", unsafe_allow_html=True)
+
 upload_resume() 
 st.divider()
 jd_button()
