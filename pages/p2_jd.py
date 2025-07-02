4005fa8d344ffe40c7d609e7941774c09c94bf35
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-02 16:25:12 +0530

    Update p2_jd.py

--- pages/p2_jd.py
+++ pages/p2_jd.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from code_to_import.jd_input import upload_jd
+from code_to_import.p2_upload_jd import upload_jd
 from styles import css_dark
 
 st.markdown(css_dark, unsafe_allow_html=True)
 upload_jd() 
+ 
