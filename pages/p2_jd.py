a4a8673a6af790bdb3daf1f4d3f03f170192d678
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 00:32:16 +0530

    Update p2_jd.py

--- pages/p2_jd.py
+++ pages/p2_jd.py
@@ -1,6 +1,9 @@
 import streamlit as st
-from pages.code_to_import.p2_upload_jd import upload_jd
 from styles import css_dark
+
+from pages.code_to_import.p2_upload_jd import upload_jd 
+from pages.code_to_import.p1_upload_resume import resume_button 
 
 st.markdown(css_dark, unsafe_allow_html=True)
 upload_jd() 
+resume_button() 
