7093b7841306e028d168df902d2c4d167022edf9
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-02 16:24:48 +0530

    Update p1_resume.py

--- pages/p1_resume.py
+++ pages/p1_resume.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from code_to_import.resume_input import upload_resume
+from code_to_import.p1_upload_resume import upload_resume
 from styles import css_dark
 
 st.markdown(css_dark, unsafe_allow_html=True)
-upload_resume()
+upload_resume() 
