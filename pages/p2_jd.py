0416861c7b57015e4b2a93ebe74defceab566bc0
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-03 01:14:43 +0530

    Update p2_jd.py

--- pages/p2_jd.py
+++ pages/p2_jd.py
@@ -4,7 +4,23 @@ from styles import css_dark
 from pages.code_to_import.p2_upload_jd import upload_jd 
 from pages.code_to_import.p1_upload_resume import resume_button 
 
-st.markdown(css_dark, unsafe_allow_html=True)
+st.markdown(css_dark, unsafe_allow_html=True) 
+st.markdown("""
+<style> 
+@media (prefers-color-scheme: dark) {
+  [data-testid="stTreeSelect"] span,
+  [data-testid="stTreeSelect"] label,
+  [data-testid="stTreeSelect"] div {
+    color: #fff !important;
+  }
+  [data-testid="stTreeSelect"] input[type="checkbox"] {
+    filter: invert(1); /* Optional: makes the checkbox white */
+  }
+} 
+</style>
+""", unsafe_allow_html=True)
+
+
 upload_jd() 
 st.divider()
 resume_button() 
