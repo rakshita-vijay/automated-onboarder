3fcdeaccb2e6ce14427beb55fc7057e03c5b843c
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-07 17:26:21 +0530

    Update p0_application_evaluator_home_page.py

--- pages/code_to_import/p0_application_evaluator_home_page.py
+++ pages/code_to_import/p0_application_evaluator_home_page.py
@@ -54,7 +54,10 @@ def app_eval_home_page():
         st.session_state.repo = None 
         
     st.divider()
-    st.markdown("### What would you like to do?", unsafe_allow_html=True) 
-    resume_button()
-    jd_button()
-    crosscheck_button()  
+    
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2: 
+        st.markdown("### What would you like to do?", unsafe_allow_html=True) 
+        resume_button()
+        jd_button()
+        crosscheck_button()  
