b51e7cb250fc3b0e6e37711916202ef58fcbb3c5
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-07-07 14:17:30 +0530

    Update p0_home_page.py

--- pages/p0_home_page.py
+++ pages/p0_home_page.py
@@ -1,1 +1,22 @@
+import streamlit as st
+from styles import css_dark 
 
+from pages.code_to_import.p0_application_evaluator_home_page import app_eval_home_page 
+
+st.markdown(css_dark, unsafe_allow_html=True)
+st.markdown("""
+<style> 
+@media (prefers-color-scheme: dark) {
+  [data-testid="stTreeSelect"] span,
+  [data-testid="stTreeSelect"] label,
+  [data-testid="stTreeSelect"] div {
+    color: #fff !important;
+  }
+  [data-testid="stTreeSelect"] input[type="checkbox"] {
+    filter: invert(1); /* Optional: makes the checkbox white */
+  }
+} 
+</style>
+""", unsafe_allow_html=True)
+
+app_eval_home_page()
