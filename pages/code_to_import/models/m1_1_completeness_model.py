e6b532ff45a6e089fedb0d130bc8da17d93472ae
Rakshita Vijay <rakshita.vijay1@gmail.com>
2025-07-27 17:01:45 +0530

    import streamlit as st added to models

--- pages/code_to_import/models/m1_1_completeness_model.py
+++ pages/code_to_import/models/m1_1_completeness_model.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 import pandas as pd
 import torch
 import joblib
--- pages/code_to_import/models/m1_2_truthiness_model.py
+++ pages/code_to_import/models/m1_2_truthiness_model.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
